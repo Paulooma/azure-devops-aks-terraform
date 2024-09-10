423627c248752f8d9f2517a0f1b2e728f3ef1c41
Paul Oyemakinwa <oyemakinwa.admin@sits.msf.org>
2024-09-10 16:23:51 +0200

    fix:change Microsoft.Containers

--- aks/module/cluster-addons.tf
+++ aks/module/cluster-addons.tf
@@ -3,7 +3,7 @@ resource "azapi_update_resource" "cluster_updates" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2024-03-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2024-06-02-preview"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
